eebb840da4d0c420213342dae9d81336d62e07b2
geekysachintech <geekysachin.info@gmail.com>
2020-11-11 19:46:56 +0530

    screen rotation added

--- app/src/main/java/com/mrmobo/camerax/MainActivity.kt
+++ app/src/main/java/com/mrmobo/camerax/MainActivity.kt
@@ -3,18 +3,19 @@ package com.mrmobo.camerax
 import android.content.pm.PackageManager
 import android.opengl.Matrix
 import android.os.Bundle
+import android.util.Log
 import android.view.Surface
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.Executor
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Executor {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,6 +45,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
+
+        val imageCaptureConfig =ImageCaptureConfig.Builder().apply {
+            setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+
+        }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        captureImage.setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file, this, object : ImageCapture.OnImageSavedListener{
+                override fun onImageSaved(file: File) {
+                    Log.i("IMAGECAPTURE", "Image Captured ${file.absolutePath}")
+//                    Toast.makeText(this@MainActivity, "Image Captured ${file.absolutePath}", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(
+                    imageCaptureError: ImageCapture.ImageCaptureError,
+                    message: String,
+                    cause: Throwable?
+                ) {
+                    Log.i("IMAGECAPTURE", "Error Capturing $message")
+//                    Toast.makeText(this@MainActivity, "Error Capturing $message", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+
        // val previewConfig :PreviewConfig.Builder = PreviewConfig.
 
        val previewConfig: PreviewConfig = PreviewConfig.Builder().apply {
@@ -64,7 +93,7 @@ class MainActivity : AppCompatActivity() {
 //            val surface = Surface(surfaceTexture)
             textureView.setSurfaceTexture(it.surfaceTexture)
         }
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, imageCapture)
 
     }
 
@@ -82,5 +111,9 @@ class MainActivity : AppCompatActivity() {
         }
         matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
         textureView.setTransform(matrix)
+    }
+
+    override fun execute(p0: Runnable?) {
+
     }
 }
